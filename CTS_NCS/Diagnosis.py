225b45265b6e0b3423f6324085bd391d405c7bd1
Ojiako <kso170000@utdallas.edu>
2023-03-07 16:15:51 -0600

    updates to the diagnosis Object and Nerve Response Object

---
+++ CTS_NCS/Diagnosis.py
@@ -0,0 +1,11 @@
+class Diagnosis:
+    def __init__(self):
+        pass
+
+    def export_file(self, file_format: str = "text"):
+        pass
+
+    def diagnosis(self, nerve_signal: NerveResponse):
+        pass
+
+
